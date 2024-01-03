dd4f32b4edd1fe740273b405aeb20e9e77c0cb8e
Ben M <makingspidersense@gmail.com>
2024-01-03 14:21:58 -0600

    Add issue details and display them on the issues.php page

--- issues.php
+++ issues.php
@@ -15,7 +15,95 @@
 <div class="container pw-20">
 
 
-My issue data
+<?php
+// Function to convert to snake_case
+function toSnakeCase($string) {
+    return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', str_replace(' ', '_', $string)));
+}
+
+// Fetch the issue key from the query string
+$requested_issue_key = isset($_GET['issue_key']) ? $_GET['issue_key'] : null;
+
+// Variables to hold issue details
+$issue_key = $summary = $issue_type = $status = $priority = $environment = $description = $creator = $created = $labels = $parent_summary = '';
+$attachments = $comments = [];
+
+if ($requested_issue_key) {
+    $file = fopen('jira.csv', 'r');
+    $headers = fgetcsv($file);
+    $snake_case_headers = array_map('toSnakeCase', $headers);
+
+    // Loop through each line of the CSV
+    while (($row = fgetcsv($file)) !== FALSE) {
+        if ($row[array_search('issue_key', $snake_case_headers)] == $requested_issue_key) {
+            // Assign values to variables
+            foreach ($snake_case_headers as $index => $header) {
+                $$header = $row[$index];
+            }
+
+            // Handling multiple attachments and comments
+            if (isset($attachments[$row[array_search('attachment_url', $snake_case_headers)]])) {
+                $attachments[] = $row;
+            }
+
+            if (isset($comments[$row[array_search('comment', $snake_case_headers)]])) {
+                $comments[] = $row[array_search('comment', $snake_case_headers)];
+            }
+
+            // No need to continue loop once the issue is found
+            break;
+        }
+    }
+    fclose($file);
+}
+?>
+
+<h2><?= htmlspecialchars($issue_key) ?> - <?= htmlspecialchars($summary) ?></h2>
+
+<section class="metadata">
+    <h3>Metadata</h3>
+    <ul>
+        <li>Issue Type: <?= htmlspecialchars($issue_type) ?></li>
+        <li>Status: <?= htmlspecialchars($status) ?></li>
+        <li>Priority: <?= htmlspecialchars($priority) ?></li>
+        <li>Environment: <?= htmlspecialchars($environment) ?></li>
+    </ul>
+</section>
+
+<section class="description">
+    <h3>Description</h3>
+    <div>
+        <?= htmlspecialchars($description) ?>
+    </div>
+</section>
+
+<section class="attachments">
+    <h3>Attachments</h3>
+    <ul>
+        <?php foreach ($attachments as $attachment): ?>
+            <li><a href="<?= htmlspecialchars($attachment[2]) ?>"><?= htmlspecialchars($attachment[3]) ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</section>
+
+<section class="more-info">
+    <h3>More Info</h3>
+    <ul>
+        <li>Creator: <?= htmlspecialchars($creator) ?></li>
+        <li>Created: <?= htmlspecialchars($created) ?></li>
+        <li>Labels: <?= htmlspecialchars($labels) ?></li>
+        <li>Parent: <?= htmlspecialchars($parent_summary) ?></li>
+    </ul>
+</section>
+
+<section class="comments">
+    <h3>Comments</h3>
+    <ul>
+        <?php foreach ($comments as $comment): ?>
+            <li><?= htmlspecialchars($comment) ?></li>
+        <?php endforeach; ?>
+    </ul>
+</section>
 
 
 </div>
