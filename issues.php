89c37a3a112ae38cf3cf58d013ed6a9becd7573a
Ben M <makingspidersense@gmail.com>
2024-01-03 14:08:58 -0600

    Add issues.php file with basic HTML structure

---
+++ issues.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PAYC-id - Paycove Jira</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="style1.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Spacing -->
+<div class="mb-50"></div>
+
+<div class="container pw-20">
+
+
+My issue data
+
+
+</div>
+
+<script src="js/app.js"></script>
+
+</body>
+</html>
